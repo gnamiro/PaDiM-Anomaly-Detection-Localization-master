f394e2b4e62cd8a6ae4270b7232d04d22d2a95c2
gnamiro <56019493+gnamiro@users.noreply.github.com>
2021-10-10 21:13:12 +0330

    Update mvtec.py
    change gr_truth tensor to zero and change class types

--- datasets/mvtec.py
+++ datasets/mvtec.py
@@ -10,13 +10,11 @@ from torchvision import transforms as T
 
 
 # URL = 'ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_anomaly_detection.tar.xz'
-CLASS_NAMES = ['bottle', 'cable', 'capsule', 'carpet', 'grid',
-               'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
-               'tile', 'toothbrush', 'transistor', 'wood', 'zipper']
+CLASS_NAMES = ['diabetic_retinopathy_detection']
 
 
 class MVTecDataset(Dataset):
-    def __init__(self, dataset_path='D:/dataset/mvtec_anomaly_detection', class_name='bottle', is_train=True,
+    def __init__(self, dataset_path='./datasets', class_name='diabetic_retinopathy_detection', is_train=True,
                  resize=256, cropsize=224):
         assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
         self.dataset_path = dataset_path
@@ -48,11 +46,11 @@ class MVTecDataset(Dataset):
         x = Image.open(x).convert('RGB')
         x = self.transform_x(x)
 
-        if y == 0:
-            mask = torch.zeros([1, self.cropsize, self.cropsize])
-        else:
-            mask = Image.open(mask)
-            mask = self.transform_mask(mask)
+        # if y == 0:
+        mask = torch.zeros([1, self.cropsize, self.cropsize])
+        # else:
+        #     mask = Image.open(mask)
+        #     mask = self.transform_mask(mask)
 
         return x, y, mask
 
@@ -75,11 +73,11 @@ class MVTecDataset(Dataset):
                 continue
             img_fpath_list = sorted([os.path.join(img_type_dir, f)
                                      for f in os.listdir(img_type_dir)
-                                     if f.endswith('.png')])
+                                     if f.endswith('.jpeg')])
             x.extend(img_fpath_list)
 
             # load gt labels
-            if img_type == 'good':
+            if img_type == '0':
                 y.extend([0] * len(img_fpath_list))
                 mask.extend([None] * len(img_fpath_list))
             else:
@@ -89,7 +87,7 @@ class MVTecDataset(Dataset):
                 gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
                                  for img_fname in img_fname_list]
                 mask.extend(gt_fpath_list)
-
+        # print("*************____>>", len(x))
         assert len(x) == len(y), 'number of x and y should be same'
 
         return list(x), list(y), list(mask)
